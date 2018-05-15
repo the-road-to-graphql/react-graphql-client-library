e5ecae1d7abaae0ed3ae9e59254390abf0ed6dcb
Robin Wieruch <wrobin@gmx.net>
2018-05-15 12:52:47 +0800

    extract client

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import MyGraphQLClient from './my-client';
+import { Provider } from './my-client-react';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new MyGraphQLClient({
+  baseURL: 'https://api.github.com/graphql',
+  headers: {
+    Authorization: `bearer ${
+      process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
+    }`,
+  },
+});
+
+ReactDOM.render(
+  <Provider value={client}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
 registerServiceWorker();
---
+++ src/my-client/index.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+class MyGraphQLClient {
+  axios;
+
+  constructor({ baseURL, headers }) {
+    this.axios = axios.create({
+      baseURL,
+      headers,
+    });
+  }
+
+  query({ query, variables }) {
+    return this.axios.post('', {
+      query,
+      variables,
+    });
+  }
+
+  mutation({ mutation, variables }) {
+    return this.axios.post('', {
+      query: mutation,
+      variables,
+    });
+  }
+}
+
+export default MyGraphQLClient;
